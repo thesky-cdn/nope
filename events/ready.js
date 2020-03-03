d52958f939af49a90e065e8dfca6d0239723cff3
thesky-cdn <56684798+thesky-cdn@users.noreply.github.com>
2020-03-03 08:18:41 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,21 @@
 module.exports = (client) => {
-  console.log(`Ready to serve in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
-  console.log("Bot Made by zvbt#5359");
-  console.log("https://zvbt.space");
-  client.user.setActivity("r!help") 
+  console.log("           ");
+  console.log("Stats:");
+  console.log(` Server: ${client.guilds.size}`);
+  console.log(` Channels: ${client.channels.size}`);
+  console.log(` Members: ${client.users.size}`);
+  console.log("Info:");
+  console.log(" Bot Made by zvbt#5359");
+  console.log(" https://zvbt.space");
+
+
+  var _channel = client.channels.find(channel => channel.id === '683851464967585792');
+
+
+  let acti = ["r!help", "Best Hentai Bot", "zvbt.space", "Made with ❤️ by zvbt#5359"], i = 0;
+
+  setInterval(function() {
+    client.user.setActivity(`${acti [i++ % acti.length]}`, {type: "STREAMING",url:'https://twitch.tv/zvbt'})
+  }, 5000)
+
 }
